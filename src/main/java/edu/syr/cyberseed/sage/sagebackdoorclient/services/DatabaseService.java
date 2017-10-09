e1dbe2d5f1e00ac480a46e716e9e25e1b124bff4
Kelly Fallon <kjfallon@syr.edu>
2017-10-08 20:26:38 -0400

    add creation of Raw Records

--- src/main/java/edu/syr/cyberseed/sage/sagebackdoorclient/services/DatabaseService.java
+++ src/main/java/edu/syr/cyberseed/sage/sagebackdoorclient/services/DatabaseService.java
@@ -1031,6 +1031,117 @@ public class DatabaseService {
 
                     }
 
+                    //
+                    //
+                    // write Raw Records to database
+                    //
+                    //
+
+                    for (edu.syr.cyberseed.sage.sagebackdoorclient.entities.xml.RawRecord record : rawRecords) {
+                        // change id from string to Integer
+                        Integer id = Integer.valueOf(record.getRecordId());
+
+                        // change dates from string to date
+                        DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+                        Date recordDate= new Date();
+                        try {
+                            recordDate = df.parse(record.getRecordDate());
+                        } catch (ParseException e) {
+                            System.out.println("RecordDate for " + id + " not in MM/DD/YYYY format");
+                        }
+
+                        //Check if API user specified supplemental users for edit or view permission
+                        List<String> xmlEditList = Arrays.asList(record.getEditPermissions().split(","));
+                        List<String> xmlViewList = Arrays.asList(record.getViewPermissions().split(","));
+                        Boolean editUsersSubmitted = ((xmlEditList != null) && (xmlEditList.size() > 0)) ? true : false;
+                        Boolean viewUsersSubmitted = ((xmlViewList != null) && (xmlViewList.size() > 0)) ? true : false;
+
+                        // create a json object of the default edit users
+                        ArrayList<String> editUserList = new ArrayList<String>();
+                        // by default do not add any users
+                        //editUserList.add(currentUser);
+                        Map<String, Object> editUserListJson = new HashMap<String, Object>();
+
+
+                        // create a json object of the default view users
+                        ArrayList<String> viewUserList = new ArrayList<String>();
+                        // by default do not add any users
+                        //viewUserList.add(currentUser);
+                        //viewUserList.add(submittedData.getPatientUsername());
+                        Map<String, Object> viewUserListJson = new HashMap<String, Object>();
+
+
+                        String finalEditPermissions = "";
+                        String finalViewPermissions = "";
+
+                        if (editUsersSubmitted) {
+                            List<String> userSuppliedListOfUsersToGrantEdit = xmlEditList;
+                            for (String username : userSuppliedListOfUsersToGrantEdit) {
+                                User possibleUser = userRepository.findByUsername(username);
+                                if ((possibleUser != null) && (StringUtils.isNotEmpty(possibleUser.getUsername()))) {
+                                    editUserList.add(username);
+                                }
+                            }
+                            editUserListJson.put("users", editUserList);
+                            JSONSerializer serializer = new JSONSerializer();
+                            finalEditPermissions = serializer.include("users").serialize(editUserListJson);
+                        }
+                        else {
+                            editUserListJson.put("users", editUserList);
+                            JSONSerializer serializer = new JSONSerializer();
+                            finalEditPermissions = serializer.include("users").serialize(editUserListJson);
+                        }
+
+                        if (viewUsersSubmitted) {
+                            List<String> userSuppliedListOfUsersToGrantView = xmlViewList;
+                            for (String username : userSuppliedListOfUsersToGrantView) {
+                                User possibleUser = userRepository.findByUsername(username);
+                                if ((possibleUser != null) && (StringUtils.isNotEmpty(possibleUser.getUsername()))) {
+                                    viewUserList.add(username);
+                                }
+                            }
+                            viewUserListJson.put("users", viewUserList);
+                            JSONSerializer serializer = new JSONSerializer();
+                            finalViewPermissions = serializer.include("users").serialize(viewUserListJson);
+                        }
+                        else {
+                            viewUserListJson.put("users", viewUserList);
+                            JSONSerializer serializer = new JSONSerializer();
+                            finalViewPermissions = serializer.include("users").serialize(viewUserListJson);
+                        }
+
+                        //logger.info("Creating records with id " + id);
+                        edu.syr.cyberseed.sage.sagebackdoorclient.entities.MedicalRecordWithoutAutoId savedMedicalRecord =
+                                medicalRecordWithoutAutoIdRepository.save(new edu.syr.cyberseed.sage.sagebackdoorclient.entities.MedicalRecordWithoutAutoId(id,
+                                        "Raw",
+                                        recordDate,
+                                        record.getOwner(),
+                                        record.getPatient(),
+                                        finalEditPermissions,
+                                        finalViewPermissions));
+                        //logger.info("Created  MedicalRecord with id " + savedMedicalRecord.getId());
+
+                        // create the raw record
+                        byte[] fileAsBytes = null;
+                        // Was the user supplied file actually base64 encoded data?
+                        try {
+                            fileAsBytes = java.util.Base64.getDecoder().decode(record.getFile());
+                        }
+                        catch (Exception e) {
+                            logger.warn("submitted an invalid base64 encoded file");
+                            e.printStackTrace();
+                        }
+                        Integer fileByteLength = fileAsBytes.length;
+                        edu.syr.cyberseed.sage.sagebackdoorclient.entities.RawRecord savedRawRecord =
+                                rawRecordRepository.save(new edu.syr.cyberseed.sage.sagebackdoorclient.entities.RawRecord(id,
+                                        record.getDescription(),
+                                        fileAsBytes,
+                                        fileByteLength));
+
+                        System.out.println("Created Raw Record with id " + savedRawRecord.getId());
+
+                    }
+
                     break;
 
                 case "getBackupCfg":
